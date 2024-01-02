acbca6d21ea73e724ec010ddca9f9b856760cef8
xiangzhongxu <xiangzhongxu@outlook.com>
2024-01-02 10:49:10 +0800

    Add

---
+++ Source/AnalyzeJson/Private/JsonComponent.cpp
@@ -0,0 +1,71 @@
+﻿// Copyright 2023 MrXiang, Inc. All Rights Reserved.
+
+
+#include "JsonComponent.h"
+
+// Sets default values for this component's properties
+UJsonComponent::UJsonComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UJsonComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UJsonComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+FJsonLib::FJsonLib()
+{
+	JsonObject = MakeShared<FJsonObject>();
+}
+
+bool FJsonLib::JsonObjectFromString(const FString& Str)
+{
+	
+	TSharedRef<TJsonReader<>> JsonReader = TJsonReaderFactory<>::Create(Str);
+	return FJsonSerializer::Deserialize(JsonReader, JsonObject);
+}
+
+bool UJsonComponent::GetJson(FString Josn,FJsonLib& OutJsonObject)
+{
+	OutJsonObject.JsonString=Josn;
+	return OutJsonObject.JsonObjectFromString(Josn);
+}
+
+void UJsonComponent::GetJsonString(FJsonLib OutJsonObject,FString Key, FString& Json)
+{
+	Json=OutJsonObject.JsonObject->GetStringField(Key);
+}
+
+void UJsonComponent::GetJsonObj(FJsonLib OutJsonObject, FString Key, FJsonLib & JsonObject)
+{
+	FJsonLib linshi;
+	// 获取Data
+	JsonObject.JsonObject = OutJsonObject.JsonObject->GetObjectField(Key);
+}
+
+void UJsonComponent::GetJsonArray(FJsonLib OutJsonObject, FString Key,TArray<FString>& NewString)
+{
+	TArray<TSharedPtr<FJsonValue>> data = OutJsonObject.JsonObject->GetArrayField(Key);
+	for(int i = 0; i < data.Num(); i++)
+	{
+		NewString.Add(data[i]->AsString());
+	}
+}
---
+++ Source/AnalyzeJson/Public/JsonComponent.h
@@ -0,0 +1,52 @@
+﻿// Copyright 2023 MrXiang, Inc. All Rights Reserved.
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "JsonComponent.generated.h"
+
+USTRUCT(BlueprintType, meta = (DisplayName = "AnalyzeJson"))
+struct  FJsonLib
+{
+	GENERATED_BODY()
+public:
+	FJsonLib();
+	FString JsonString;
+	TSharedPtr<FJsonObject> JsonObject;
+
+	bool JsonObjectFromString(const FString& Str);
+};
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class ANALYZEJSON_API UJsonComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UJsonComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,FActorComponentTickFunction* ThisTickFunction) override;
+
+public:
+	FJsonLib NewFJsonLib;
+	//转换
+	UFUNCTION(BlueprintCallable,Category="AnalyzeJson")
+	bool GetJson(FString Json,FJsonLib& OutJsonObject);
+	//获取对象指定的Key
+	UFUNCTION(BlueprintCallable,Category="AnalyzeJson")
+	void GetJsonString(FJsonLib OutJsonObject,FString Key,FString& Json);
+	//获取对象中的对象
+	UFUNCTION(BlueprintCallable,Category="AnalyzeJson")
+	void GetJsonObj(FJsonLib OutJsonObject,FString Key,FJsonLib & JsonObject);
+	UFUNCTION(BlueprintCallable,Category="AnalyzeJson")
+	void GetJsonArray(FJsonLib OutJsonObject,FString Key,TArray<FString>& NewString);
+};
